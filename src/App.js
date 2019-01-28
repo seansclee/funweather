b90c59ce474b0c7a6403e3e2985e0a90d3c38057
Sean Lee <seanslee92@gmail.com>
2019-01-28 10:40:02 -0500

    banner formatted

--- src/App.js
+++ src/App.js
@@ -14,79 +14,85 @@ class App extends Component {
   state = { activeItem: 'home', phoneVisible: false }
 
   componentDidMount() {
-    setTimeout(() => this.setState({ ...this.state, phoneVisible: true }), 250)
+    setTimeout(() => this.setState({ ...this.state, phoneVisible: true }), 500)
   }
 
   handleItemClick = (e, { name }) => this.setState({ ...this.state, activeItem: name })
 
-  scrollTo = () => Scroll.animateScroll.scrollTo(600)
+  scrollTo = () => Scroll.animateScroll.scrollTo(1000)
 
   render() {
     const { activeItem, phoneVisible } = this.state
     return (
       <div className='App'>
-        <Container style={{ paddingRight: '10em', paddingLeft: '10em' }}>
-          <Menu inverted text size='mini'>
+        <Container>
+          <Menu inverted text size='small'>
             <Menu.Item position='left' link href='/'>
               <Image size='small' src={logo} />
             </Menu.Item>
             <Menu.Menu>
               <Menu.Item
-                style={{ marginRight: '2.5em' }}
+                style={{ marginRight: '3.5em' }}
                 name='home'
                 active={activeItem === 'home'}
                 onClick={this.handleItemClick}
               />
               <Menu.Item
-                style={{ marginLeft: '2.5em', marginRight: '2.5em' }}
+                style={{ marginLeft: '3em', marginRight: '3em' }}
                 name='features'
                 active={activeItem === 'features'}
                 onClick={this.handleItemClick}
               />
               <Menu.Item
-                style={{ marginLeft: '2.5em', marginRight: '2.5em' }}
+                style={{ marginLeft: '3em', marginRight: '3em' }}
                 name='reviews'
                 active={activeItem === 'reviews'}
                 onClick={this.handleItemClick}
               />
               <Menu.Item
-                style={{ marginLeft: '2.5em', marginRight: '2.5em' }}
+                style={{ marginLeft: '3em', marginRight: '3em' }}
                 name='download'
                 active={activeItem === 'download'}
                 onClick={this.handleItemClick}
               />
             </Menu.Menu>
             <Menu.Menu relaxed position='right'>
-                <Menu.Item>
-                  <Icon size='large' link name='facebook f' />
-                </Menu.Item>
-                <Menu.Item>
-                  <Icon size='large' link name='instagram' />
-                </Menu.Item>
-                <Menu.Item>
-                  <Icon size='large' link name='twitter' />
-                </Menu.Item>
-              </Menu.Menu>
+              <Menu.Item href='https://www.facebook.com'>
+                <Icon size='large' name='facebook f' />
+              </Menu.Item>
+              <Menu.Item href='https://www.instagram.com'>
+                <Icon size='large' name='instagram' />
+              </Menu.Item>
+              <Menu.Item href='https://www.twitter.com'>
+                <Icon size='large' name='twitter' />
+              </Menu.Item>
+            </Menu.Menu>
           </Menu>
         </Container>
-        <Container style={{ height: '600px', paddingRight: '10em', paddingLeft: '10em' }}>
+        <Container style={{ height: '50vw' }}>
           <Grid>
             <Grid.Row columns={2}>
               <Grid.Column>
-                <Header as='h1' style={{ color: 'white', fontSize: '2.3em', paddingTop: '3em' }}>
+                <Header as='h1' style={{ color: 'white', fontSize: '3vw', paddingTop: '11vw' }}>
                   <Header.Content>
                     Get the most fun weather app
-                    <Header.Subheader style={{ color: 'white', padding: '2em 7em 1.5em 0em', fontSize: '.3em', lineHeight: '1.8em' }}>
+                    <Header.Subheader style={{ color: 'white', padding: '2vw 10vw 2vw 0em', fontSize: '.3em', lineHeight: '1.8em' }}>
                       Simple, nice and user-friendly application of the weather. Only useful information
                     </Header.Subheader>
                   </Header.Content>
                 </Header>
-                <Button size='medium' color='orange'>Download</Button>
-                <Button size='medium' color='blue'>Features</Button>
+                <Container style={{ marginBottom: '11vw' }}>
+                  <Button size='big' color='orange'>Download</Button>
+                  <Button size='big' color='blue'>Features</Button>
+                </Container>
               </Grid.Column>
               <Grid.Column>
-                <Transition visible={phoneVisible} animation={'fade up'} duration={500}>
-                  <Image centered src={phone} style={{ height: '400px' }} />
+                <Transition visible={phoneVisible} animation={'fade up'} duration={1000}>
+                  <Image
+                    centered
+                    src={phone}
+                    style={{ width: '18vw', paddingTop: '6vw', paddingRight: '3vw' }}
+                  />
                 </Transition>
                 <div class='bubble orange'>SUNNY</div>
                 <div class='bubble blue'>LONDON</div>
@@ -99,18 +105,20 @@ class App extends Component {
             <Icon style={{ margin: 'auto' }} name='chevron down' />
           </div>
         </Container>
-        <Header color='pink' as='h1' textAlign='center'>PERFECT FEATURES</Header>
-        <Header color='brown' as='h4' textAlign='center'>Only necessary</Header>
-        <Container style={{ padding: '10em 8em 20em' }}>
+        <Container style={{ paddingTop: '5.5vw' }}>
+          <Header color='pink' as='h1' textAlign='center' style={{ fontSize: '1.8vw' }}>PERFECT FEATURES</Header>
+          <Header color='brown' as='h4' textAlign='center'>Only necessary</Header>
+        </Container>
+        <Container style={{ padding: '8vw 0vw 23.2vw' }}>
           <Grid equal centered columns={6}>
             <Grid.Column width={2}>
               <Image floated='right' src={icon1} />
             </Grid.Column>
             <Grid.Column width={3}>
               <Header as='h3'>
-                <Header.Content style={{ color: '#353854' }}>
+                <Header.Content style={{ color: '#353854', fontSize: '1.2vw' }}>
                   Usability
-                  <Header.Subheader style={{ color: 'aeaeae', fontSize: '.7em', lineHeight: '1.8em' }}>
+                  <Header.Subheader style={{ color: 'aeaeae', fontSize: '1vw', lineHeight: '1.8em' }}>
                     Sometimes the simplest things are the hardest to find. So we created a new line for everyday life
                   </Header.Subheader>
                 </Header.Content>
@@ -121,7 +129,7 @@ class App extends Component {
             </Grid.Column>
             <Grid.Column width={3}>
               <Header as='h3'>
-                <Header.Content style={{ color: '#353854' }}>
+                <Header.Content style={{ color: '#353854', fontSize: '1.2vw' }}>
                   Parallax Effect
                   <Header.Subheader style={{ color: 'aeaeae', fontSize: '.7em', lineHeight: '1.8em' }}>
                     Sometimes the simplest things are the hardest to find. So we created a new line for everyday life
@@ -134,7 +142,7 @@ class App extends Component {
             </Grid.Column>
             <Grid.Column width={3}>
               <Header as='h3'>
-                <Header.Content style={{ color: '#353854' }}>
+                <Header.Content style={{ color: '#353854', fontSize: '1.2vw' }}>
                   Unlimites Colors
                   <Header.Subheader style={{ color: 'aeaeae', fontSize: '.7em', lineHeight: '1.8em' }}>
                     Sometimes the simplest things are the hardest to find. So we created a new line for everyday life
@@ -146,45 +154,45 @@ class App extends Component {
         </Container>
         <Segment color='purple' inverted style={{ padding: '2em 0em 2em' }}>
           <Container style={{ paddingRight: '10em', paddingLeft: '10em' }}>
-            <Menu inverted text size='mini'>
+            <Menu inverted text size='small'>
               <Menu.Item position='left' link href='/'>
                 <Image size='small' src={coloredLogo} />
               </Menu.Item>
               <Menu.Menu>
                 <Menu.Item
-                  style={{ marginRight: '2.5em' }}
+                  style={{ marginRight: '3.5em' }}
                   name='home'
                   active={activeItem === 'home'}
                   onClick={this.handleItemClick}
                 />
                 <Menu.Item
-                  style={{ marginLeft: '2.5em', marginRight: '2.5em' }}
+                  style={{ marginLeft: '3em', marginRight: '3em' }}
                   name='features'
                   active={activeItem === 'features'}
                   onClick={this.handleItemClick}
                 />
                 <Menu.Item
-                  style={{ marginLeft: '2.5em', marginRight: '2.5em' }}
+                  style={{ marginLeft: '3em', marginRight: '3em' }}
                   name='reviews'
                   active={activeItem === 'reviews'}
                   onClick={this.handleItemClick}
                 />
                 <Menu.Item
-                  style={{ marginLeft: '2.5em', marginRight: '2.5em' }}
+                  style={{ marginLeft: '3em', marginRight: '3em' }}
                   name='download'
                   active={activeItem === 'download'}
                   onClick={this.handleItemClick}
                 />
               </Menu.Menu>
               <Menu.Menu relaxed position='right'>
-                <Menu.Item>
-                  <Icon size='large' link name='facebook f' />
+                <Menu.Item href='https://www.facebook.com'>
+                  <Icon size='large' name='facebook f' />
                 </Menu.Item>
-                <Menu.Item>
-                  <Icon size='large' link name='instagram' />
+                <Menu.Item href='https://www.instagram.com'>
+                  <Icon size='large' name='instagram' />
                 </Menu.Item>
-                <Menu.Item>
-                  <Icon size='large' link name='twitter' />
+                <Menu.Item href='https://www.twitter.com'>
+                  <Icon size='large' name='twitter' />
                 </Menu.Item>
               </Menu.Menu>
             </Menu>
